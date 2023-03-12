1bc9c716dbc0fea01020182127108082ab2babcc
Daniel <dfernandez2993@gmail.com>
2023-03-12 00:22:37 -0300

    Comienzo del proyecto del taller mecanico. Application Properties configurado

---
+++ src/test/java/ar/com/besysoft/tallermecanico/TallerMecanicoApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.besysoft.tallermecanico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerMecanicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
